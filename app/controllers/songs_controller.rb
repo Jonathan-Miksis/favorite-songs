9940c3e75b115802e70bb847c87fcea4b59e7ae5
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-18 19:29:46 -0400

    add create, update, and destroy actions/routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,37 @@ class SongsController < ApplicationController
     render json: {message: "hello"}
   end
 
+  def show 
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    song.save
+    render json: song.as_json
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title]
+    song.album = params[:album]
+    song.artist = params[:artist]
+    song.year = params[:year]
+    song.save
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.destroy
+    render json: {message: "You have successfully removed this song from your library."}
+  end
+
+
 end
